527a8195d7949cc6c12cded3b590b17da4d1e3a3
lucaesse <75520785+lucaesse@users.noreply.github.com>
2025-06-06 15:33:47 +0200

    extension_attribute.sh

---
+++ Automatic Admin Password Changer/extension_attribute.sh
@@ -0,0 +1,23 @@
+##############################################
+# Created by lucaesse                        #
+# https://github.com/lucaesse/Jamf-McNuggets #
+##############################################
+#!/bin/bash
+
+# Path to the encrypted file
+file="/private/var/tmp/encrypted_localadmin_password.txt"
+key="123456"  # Encryption key
+
+# Check if the file exists and contains data
+if [ -f "$file" ] && [ -s "$file" ]; then
+    encrypted=$(cat "$file")
+    # Decrypt the password
+    decrypted=$(echo "$encrypted" | openssl enc -aes-256-cbc -a -d -salt -pass pass:"$key" 2>/dev/null)
+    if [ -n "$decrypted" ]; then
+        echo "<result>$decrypted</result>"
+    else
+        echo "<result>Decryption Failed</result>"
+    fi
+else
+    echo "<result>Not Set</result>"
+fi
